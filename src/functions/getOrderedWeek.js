eae8de0a868b67d5f29312021a423a122b32120b
nicomigueles <nicolasmigueles99@gmail.com>
2020-02-10 03:52:41 -0300

    quick fix

--- src/functions/getOrderedWeek.js
+++ src/functions/getOrderedWeek.js
@@ -1,21 +1,6 @@
-/**
- * @returns {Number}
- */
 export const today = new Date().getDay();
 
-/**
- * Devuelve la semana ordenda segun el dia que es.
- * Por ejemplo, si es Martes ( indice 2, ya que el 0 es el domingo, 1 lunes, etc...)
- * entonces devuelve [2,3,4,5,6,0,1]
- * @returns {Number[]}
- */
-export const getOrderedWeek = () => {
-  const weekOrdered = [];
-  for (let i = 0; i < 7; i += 1) {
-    weekOrdered.push((today + i) % 7);
-  }
-  return weekOrdered;
-};
+export const WeekFuture = ['Hoy', 'Mañana'];
 
 export const WeekDays = [
   'Domingo',
@@ -27,6 +12,12 @@ export const WeekDays = [
   'Sabado',
 ];
 
-export const WeekFuture = ['Hoy', 'Mañana'];
+export const getOrderedWeek = () => {
+  const weekOrdered = [];
+  for (let i = 0; i < 7; i += 1) {
+    weekOrdered.push((today + i) % 7);
+  }
+  return weekOrdered;
+};
 
 export const Week = getOrderedWeek();
